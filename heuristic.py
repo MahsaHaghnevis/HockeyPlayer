be218a26232de24aee87b06e2da9f132e1297ba3
Mahsa <haghnevismahsa@gmail.com>
2024-11-02 02:18:25 +0330

    feat : calculate eulides distance and then check wether its less than the others

--- heuristic.py
+++ heuristic.py
@@ -3,7 +3,8 @@ import math
 def heuristic(playerPosition, balls , goals):
    nearestBallDistance = math.inf
    for ball in balls:
-       
+       distance = math.sqrt((playerPosition[0] - ball[0])**2 + (playerPosition[1] - ball[1])**2)
+       nearestBallDistance = min(nearestBallDistance , distance)
    
    
    nearestGoalDistance = math.inf
